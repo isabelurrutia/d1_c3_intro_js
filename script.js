ef31b15f0265f7376cfbb36315a7c77f019b2ed0
Isabel Urrutia <profesora.isabel.u.a@gmail.com>
2024-06-09 17:43:43 -0400

    arreglos

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio;
 
 let cantidad = 0;
-/*function agrega_uno(){
-    let elemento = document.querySelector("#cantidad");
-    sum = sum + 1;
-    elemento.innerText =  sum;
-*/
 
 function agrega_uno(){
     cantidad = cantidad + 1;
